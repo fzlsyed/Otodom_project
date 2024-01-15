344774293c2a732d40952242742781fc35045f46
Fazlur Rahman Syed <syedfazlur04@gmail.com>
2024-01-15 13:13:51 -0500

    Update Address.py

--- Address.py
+++ Address.py
@@ -1,5 +1,3 @@
-// This Python code flattens the longitude and latitude data using an SQL Query and then coverts them into an address 
-
 import pandas as pd
 import numpy as np
 from geopy.geocoders import Nominatim
@@ -21,7 +19,9 @@ engine = create_engine(URL(
                     database = 'demo',
                     schema = 'public',
                     warehouse = 'demo_wh'))
-                
+
+# Runs an SQL query in SnowflakeSQL that flattens latitude and longitude data
+
 with engine.connect() as conn:
     try:
         query = """ SELECT RN, concat(latitude,',',longitude) as LOCATION
@@ -31,21 +31,34 @@ with engine.connect() as conn:
                         FROM otodom_data_flatten WHERE rn between 1 and 1000
                         ORDER BY rn  ) """
         print("--- %s seconds ---" % (time.time() - start_time))
+
+        # Loads data into Pandas data frame 'df'
         
         df = pd.read_sql(query,conn)
-                      
+
+        # Converts columns into uppercase to prep for to_sql function
+        
         df.columns = map(lambda x: str(x).upper(), df.columns)
+
+        # Loads data from pandas into dask.dataframe 'dd' which enables multiple pandas data frames to run simultaneously 
         
         ddf = dd.from_pandas(df,npartitions=10)
         print(ddf.head(5,npartitions=-1))
-
+        
+        # Creates new column 'Address' and passes latitude and longitude data into geolocator API  which then converts the data
+        # into country, city, zip code, etc.. 
+        
         ddf['ADDRESS'] = ddf['LOCATION'].apply(lambda x: geolocator.reverse(x).raw['address'],meta=(None, 'str'))
         print("--- %s seconds ---" % (time.time() - start_time))
 
+        # Moves data from dask_dataframe back into pandas
+        
         pandas_df = ddf.compute()
         print(pandas_df.head())
         print("--- %s seconds ---" % (time.time() - start_time))
 
+        # Loads data from pandas into SnowflakeSQL
+        
         pandas_df.to_sql('otodom_data_flatten_address', con=engine, if_exists='append', index=False, chunksize=16000, method=pd_writer)
     except Exception as e:
         print('--- Error --- ',e)
